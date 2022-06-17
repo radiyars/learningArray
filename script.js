f29dfa10622fea03e84cc41711fc4389bdb0ca55
Radiy <radiyars@yandex.ru>
2022-06-17 11:07:56 +0300

    learningArray. Task 9

--- script.js
+++ script.js
@@ -245,3 +245,39 @@ let usersMapped = users1.map(item => {
 
 console.log(usersMapped);
 console.log('---------------------------------------');
+
+
+//?_8_Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+// Например:
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+
+let vasya2 = { name: "Вася", age: 25 };
+let petya2 = { name: "Петя", age: 30 };
+let masha2 = { name: "Маша", age: 28 };
+
+let arr2 = [vasya, petya, masha];
+
+function sortByAge(arr) {
+	arr.sort((a, b) => a.age - b.age);
+	// arr.sort((a, b) => a.age > b.age ? 1 : -1);
+}
+
+sortByAge(arr2)
+
+console.log(arr2[0].name); // Вася
+console.log(arr2[1].name); // Маша
+console.log(arr2[2].name); // Петя
