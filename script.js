1c59029e1dcc3561b24c5e9a2cf50a7b8eb2703c
Radiy <radiyars@yandex.ru>
2022-06-17 10:52:44 +0300

    learningArray. Task 7

--- script.js
+++ script.js
@@ -192,3 +192,56 @@ let users = [vasya, petya, masha];
 let names = users.map(item => item.name)
 
 console.log(names);
+console.log('---------------------------------------');
+
+
+//?_(7)_У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+// Например:
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = /* ... ваш код ... */
+
+// 
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// 
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+// Итак, на самом деле вам нужно трансформировать один массив объектов в другой. Попробуйте использовать =>. Это небольшая уловка.
+
+
+let vasya1 = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya1 = { name: "Петя", surname: "Иванов", id: 2 };
+let masha1 = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users1 = [vasya1, petya1, masha1];
+
+let usersMapped = users1.map(item => {
+	item.fullName = item.name + ' ' + item.surname
+	item.id = item.id;
+	delete item.name;
+	delete item.surname;
+	return item
+});
+
+//* вариант решения:
+// let usersMapped = users1.map(user => ({
+// 	fullName: `${user.name} ${user.surname}`,
+// 	id: user.id
+// }));
+//*-----------------
+
+console.log(usersMapped);
+console.log('---------------------------------------');
